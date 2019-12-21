d2efc62ec17a35d8c8073986e2a2ab7c020633a7
Adrian Roskovshenko <ad.roskov@gmail.com>
2019-12-21 11:50:30 +0200

    FL_HW6
    Completed first task

---
+++ FL12_HW6/homework/js/task1.js
@@ -0,0 +1,39 @@
+alert(
+    'Hi! Please enter 3 numbers (a, b, c) for quadratic equation \n (a*x^2 + b*x + c = 0)'
+);
+
+var a = parseFloat(prompt('Enter number a'));
+var b = parseFloat(prompt('Enter number b'));
+var c = parseFloat(prompt('Enter number c'));
+var d;
+var x1;
+var x2;
+
+if (a === 0 || a === '' || isNaN(a)) {
+    console.log('Invalid input data');
+} else {
+    console.log('a = ' + a);
+}
+if (b === 0 || b === '' || isNaN(b)) {
+    console.log('Invalid input data');
+} else {
+    console.log('b = ' + b);
+}
+if (c === 0 || c === '' || isNaN(c)) {
+    console.log('Invalid input data');
+} else {
+    console.log('c = ' + c);
+}
+
+d = Math.pow(b, 2) - 4 * a * c;
+console.log('Discriminant = ' + d);
+if (d < 0) {
+    console.log('Discriminant < 0. No solution');
+} else if (d === 0) {
+    x1 = -b / 2 * a;
+    console.log('Discriminant = 0. x1 =' + x1);
+} else {
+    x1 = (-b + Math.sqrt(d) / 2) * a;
+    x2 = (-b - Math.sqrt(d) / 2) * a;
+    console.log('Discriminant > 0. x1 =' + x1, ';', 'x2 = ' + x2);
+}
